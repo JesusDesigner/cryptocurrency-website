92774b534b92738129fe2bde4a8e8d8ea34d47b7
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-07-18 20:39:05 +0300

    added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,28 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      primary: 'Rubik',
+    },
+    extend: {
+      colors: {
+        darkblue: {
+          DEFAULT: '#0D0D2B',
+          secondary: '#252540',
+        },
+        blue: {
+          DEFAULT: '#3671E9',
+          hover: '#2766E6',
+        },
+        violet: '#2B076E',
+      },
+    },
+    screens: {
+      // sm: '640px',
+      // md: '768px',
+      // lg: '1024px',
+      xl: '1200px',
+    },
   },
   plugins: [],
 };
