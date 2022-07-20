77af28270acdc9052984105a9760d2ad02ea8ebc
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-07-20 13:29:54 +0300

    added

--- src/components/Calculate.js
+++ src/components/Calculate.js
@@ -5,8 +5,14 @@ const Calculate = () => {
     <section className='section'>
       {/* text */}
       <div className='text-center'>
-        <h2 className='section-title'>Check how much you can earn</h2>
-        <p className='section-subtitle text-lg max-w-[622px] mx-auto'>
+        <h2 className='section-title' data-aos='fade-up' data-aos-offset='400'>
+          Check how much you can earn
+        </h2>
+        <p
+          className='section-subtitle text-lg max-w-[622px] mx-auto'
+          data-aos='fade-up'
+          data-aos-offset='450'
+        >
           Let’s check your hash rate to see how much you will earn today,
           Exercitation veniam consequat sunt nostrud amet.
         </p>
