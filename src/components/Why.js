e5c6a2901ef8a683d00cfaab28f7c0f8c564fb35
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-07-20 12:31:53 +0300

    added

--- src/components/Why.js
+++ src/components/Why.js
@@ -7,13 +7,13 @@ const Why = () => {
   return (
     <section>
       <div className='container mx-auto'>
-        <div className='flex flex-col items-center lg:flex-row'>
+        <div className='flex flex-col items-center gap-x-8 lg:flex-row'>
           {/* image */}
           <div className='order-2 lg:order-1'>
             <img src={Image} alt='' />
           </div>
           {/* text */}
-          <div className='order-1 lg:order-2'>
+          <div className='order-1 lg:order-2 max-w-[480px]'>
             <h2 className='section-title'>Why you should choose CRAPPO</h2>
             <p className='section-subtitle'>
               Experience the next generation cryptocurrency platform. No
