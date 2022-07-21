a835f5dad1a6686f4920e84da8febf99d97e2a27
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-07-21 14:03:10 +0300

    added

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
-// import images
-import Image1 from '../assets/img/feature-1-img.png';
+// import components
+import FeaturesSection1 from './FeaturesSection1';
+import FeaturesSection2 from './FeaturesSection2';
+import FeaturesSection3 from './FeaturesSection3';
 
 const Features = () => {
   return (
@@ -17,29 +19,9 @@ const Features = () => {
             choice
           </h2>
         </div>
-        <div className='flex flex-col lg:flex-row'>
-          {/* text */}
-          <div
-            className='max-w-[454px] mb-6 lg:mt-10'
-            data-aos='fade-right'
-            data-aos-offset='400'
-          >
-            <h3 className='h3 mb-6'>Invest Smart</h3>
-            <p className='text-gray mb-8'>
-              Get full statistic information about the behaviour of buyers and
-              sellers will help you to make the decision.
-            </p>
-            <button className='btn px-8'>Learn more</button>
-          </div>
-          {/* image */}
-          <div
-            className='flex-1 flex justify-end'
-            data-aos='fade-left'
-            data-aos-offset='400'
-          >
-            <img src={Image1} alt='' />
-          </div>
-        </div>
+        <FeaturesSection1 />
+        <FeaturesSection2 />
+        <FeaturesSection3 />
       </div>
     </section>
   );
